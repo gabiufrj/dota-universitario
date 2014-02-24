c8749e887c3f65bb634bc543f0eef1aa67c309fc
gabriel <gabpires21@gmail.com>
2014-02-24 01:38:59 -0300

    Minor fixes nos templates de campeonatos e times

--- DotaUniversitario/settings.py
+++ DotaUniversitario/settings.py
@@ -1,6 +1,6 @@
 # Django settings for DotaUniversitario project.
 
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -166,7 +166,7 @@ LOGGING = {
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
 # DESCOMENTAR A LINHA ABAIXO PARA DEPLOY
-DATABASES['default'] =  dj_database_url.config()
+#DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
